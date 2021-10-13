5c0d0e043d3af929f0dc4868da8866f92f3f998a
metrupki <67233660+metrupki@users.noreply.github.com>
2021-10-14 00:13:27 +0200

    fixed cards issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,12 @@ import { StaticImage } from "gatsby-plugin-image"
 import * as React from "react"
 import Layout from "../components/Layout"
 import * as blogStyle from "./index.module.scss"
+import { Card, Heading } from 'rebass'
 
 const IndexPage = () => {
   return (
     <Layout>
         <div className={blogStyle.cards}>
-          <div className={blogStyle.cardItem}>
-            <div className={blogStyle.text}>
-              <h1>This is the title of this</h1>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fungit blanditiis quia accusamus harum molestiae voluptate soluta.</p>
-            </div>
-            <div className={blogStyle.picture}>
-              <StaticImage src="../images/photo1.jpg" />
-            </div>
-          </div>
 
           <div className={blogStyle.cardItem}>
             <div className={blogStyle.text}>
@@ -26,6 +18,15 @@ const IndexPage = () => {
               <StaticImage src="../images/photo1.jpg" />
             </div>
           </div>
+          <div className={blogStyle.cardItem}>
+            <div className={blogStyle.text}>
+              <h1>This is the title of this</h1>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fungit blanditiis quia accusamus harum molestiae voluptate soluta.</p>
+            </div>
+            <div className={blogStyle.picture}>
+              <StaticImage src="../images/gaming.png" />
+            </div>
+          </div>
 
           <div className={blogStyle.cardItem}>
             <div className={blogStyle.text}>
@@ -33,7 +34,7 @@ const IndexPage = () => {
               <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fungit blanditiis quia accusamus harum molestiae voluptate soluta.</p>
             </div>
             <div className={blogStyle.picture}>
-              <StaticImage src="../images/photo1.jpg" />
+              <StaticImage src="../images/food.png" />
             </div>
           </div>
         </div>
